a6800aa6f0b94c61410795cc468e1f7e28d6b17b
hofffman <hofffmancx2017@gmail.com>
2017-04-18 11:08:08 +0800

    implement part 2

---
+++ db/migrate/20170417151920_add_is_admin_to_user.rb
@@ -0,0 +1,11 @@
+#class AddIsAdminToUser < ActiveRecord::Migration[5.0]
+#  def change
+#    add_column :users, :is_admin, :boolean, default: false
+#  end
+# end
+
+class AddIsAdminToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :is_admin, :boolean, default: false
+  end
+end
