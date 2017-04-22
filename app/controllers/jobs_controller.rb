23da0551e1907c61bc81909c53f1dc0c68e134ca
duzilong <hofffmancx2017@gmail.com>
2017-04-22 17:05:30 +0800

    achieve the navbar, insert search-box

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :validate_search_key, only: [:search]
 
   def index
     @jobs = case params[:order]
@@ -55,6 +56,29 @@ class JobsController < ApplicationController
     redirect_to jobs_path
   end
 
+#搜索框，戴建林
+
+  def search
+      if @query_string.present?
+        search_result = Job.ransack(@search_criteria).result(:distinct => true)
+        @jobs = search_result.paginate(:page => params[:page], :per_page => 5 )
+      end
+    end
+
+
+    protected
+
+    def validate_search_key
+      @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+      @search_criteria = search_criteria(@query_string)
+    end
+
+
+    def search_criteria(query_string)
+      { :title_cont => query_string }
+    end
+
+# -end 搜索框完毕
 
 
   private
